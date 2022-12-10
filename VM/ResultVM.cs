e375d2f53a9eab11156bd997136010fc3607cfa2
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2022-12-10 22:58:20 +0200

    result page sides output

--- VM/ResultVM.cs
+++ VM/ResultVM.cs
@@ -1,10 +1,29 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using Microsoft.Maui.Controls.Shapes;
+using GeoLab_Proj.Geom;
 
 namespace GeoLab_Proj
 {
     public partial class ResultVM : ObservableObject
     {
-        
+        int sidesCount = 0;
+
+        [ObservableProperty]
+        string figureType;
+        [ObservableProperty]
+        List<double> allSides = new();
+        [ObservableProperty]
+        List<string> sidesNames = new(); 
+
+        public ResultVM()
+        {
+            OutputBaseInfo();
+        }
+
+        public void OutputBaseInfo()
+        {
+            FigureType =  Figure.FigureType().ToString();
+            sidesCount = Figure.Points.Count;
+            // allSides = 
+        }
     }
 }
